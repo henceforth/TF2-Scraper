e6f9fc5d7c4556abe6603feb7049ace49510569f
hcf <electriclarryland@gmail.com>
2014-07-24 17:11:27 +0200

    refactored fixed regex for cost

--- trader.py
+++ trader.py
@@ -9,81 +9,93 @@ logging.basicConfig()
 l = logging.getLogger(__name__)
 l.setLevel(logging.DEBUG)
 
-SCRAP_BASE_URL = "http://scrap.tf"
-PERSIST_TO_SHELVE = True
-SHELVE_NAME = "items.db"
-
 def writePage(content):
   with file("/media/tmp/out.html", "w") as f:
     l.debug("writing page content")
     f.write(content.encode("utf8"))
 
-if __name__ == "__main__":
+class ScrapTfScraper(object):
+  SCRAP_BASE_URL = "http://scrap.tf"
+  PERSIST_TO_SHELVE = True
+  SHELVE_NAME = "items.db"
+
   regLvl = re.compile("Level: (?P<level>[0-9]*[0-9]*[0-9]+)")
-  regCost = re.compile(".*Costs: (?P<cost>.+)<br/>")
+  regCost = re.compile(".*Costs:? (?P<cost>.+)<br/>")
 
-  l.info("getting log-in page")
+  session = requests.Session()
+  items = {}
 
-  s = requests.Session()
-  s.cookies["steamLogin"] = "76561197960710974%7C%7C0FFC75DC5F2151BCB52D9B945E78C31BA6DE4B84"
-  s.cookies["steamLoginSecure"] = "76561197960710974%7C%7C8E9D8F235B4D350E2BD8A562758C6D77E29CA5DF"
-  s.cookies["steamMachineAuth76561197960710974"] = "F2FCF7794033272A414D5348F15D2B6DEC2A14D2"
+  def __init__(self):
+    self.session.cookies["steamLogin"] = "76561197960710974%7C%7C0FFC75DC5F2151BCB52D9B945E78C31BA6DE4B84"
+    self.session.cookies["steamLoginSecure"] = "76561197960710974%7C%7C8E9D8F235B4D350E2BD8A562758C6D77E29CA5DF"
+    self.session.cookies["steamMachineAuth76561197960710974"] = "F2FCF7794033272A414D5348F15D2B6DEC2A14D2"
+    self.__login()
+    self.scrapeItems()
 
-  r = s.get(SCRAP_BASE_URL + "/login")
-  payload = {}
+    if self.PERSIST_TO_SHELVE:
+      self.persistItems()
 
-  soup = BeautifulSoup(r.text)
-  openIdForm = soup.find(id="openidForm")
+    l.info("%i items" % len(self.items))
 
-  if openIdForm == None:
-    l.error("can't find the login form, aborting")
-    quit()
-
-  for n in openIdForm.find_all("input"):
-    if "name" in n.attrs.keys():
-      l.debug("adding to payload: %s -  %s" % (n["name"], n["value"]))
-      payload[n["name"]] = n["value"]
-
-  r = s.post("https://steamcommunity.com/openid/login", data=payload)
-  l.info("url: %s" % r.url)
-
-  if r.url.startswith(SCRAP_BASE_URL):
-    l.info("LOGIN SUCCESS!")
-  else:
-    l.error("LOGIN FAIL, url doesn't seem to be scrap.tf")
-    quit()
-
-  pages = []
-  l.info("looking for pages...")
-  r = s.get(SCRAP_BASE_URL + "/stranges")
-
-  soup = BeautifulSoup(r.text)
-  for n in soup.find_all(class_="bank-selector-box"):
-    pages.append(n.find("a")["href"])
-
-  l.debug("found %i pages" % len(pages))
-
-  objects = {}
-  for page in pages:
-    r = s.get(SCRAP_BASE_URL + page)
-    soup = BeautifulSoup(r.text)
-
-    for n in soup.find_all("div"):
-      if "data-title" in n.attrs.keys():
-        if n["data-title"] not in objects.keys():
-          lvl = regLvl.match(n["data-content"]).group("level")
-          cost = regCost.match(n["data-content"]).group("cost")
-          objects[n["data-title"]] = {"cost": cost, "quanitity": 1}
-        else:
-          objects[n["data-title"]]["quanitity"] += 1
-
-  for k, v in objects.iteritems():
-    print "%s: %s, %ix" % (k, v["cost"], v["quanitity"])
-
-  if PERSIST_TO_SHELVE:
-    l.info("persisting")
-    d = shelve.open(SHELVE_NAME)
-    d["items"] = objects
+  def persistItems(self):
+    l.info("persisting as %s" % self.SHELVE_NAME) 
+    d = shelve.open(self.SHELVE_NAME)
+    d["items"] = self.items
     d.close()
 
-  l.info("%i items" % len(objects))
+  def dumpItems(self):
+    for k, v in self.items.iteritems():
+      print "%s: %s, %ix" % (k, v["cost"], v["quanitity"])
+
+  def scrapeItems(self):
+    pages = []
+    l.info("looking for pages...")
+    r = self.session.get(self.SCRAP_BASE_URL + "/items")
+
+    soup = BeautifulSoup(r.text)
+    for n in soup.find_all(class_="bank-selector-box"):
+      pages.append(n.find("a")["href"])
+
+    l.debug("found %i pages" % len(pages))
+
+    for page in pages:
+      r = self.session.get(self.SCRAP_BASE_URL + page)
+      soup = BeautifulSoup(r.text)
+
+      for n in soup.find_all("div"):
+        if "data-title" in n.attrs.keys():
+          if n["data-title"] not in self.items.keys():
+            lvl = self.regLvl.match(n["data-content"]).group("level")
+            cost = self.regCost.match(n["data-content"]).group("cost")
+            self.items[n["data-title"]] = {"cost": cost, "quanitity": 1}
+          else:
+            self.items[n["data-title"]]["quanitity"] += 1
+
+  def __login(self):
+    l.info("getting log-in page")
+    r = self.session.get(self.SCRAP_BASE_URL + "/login")
+    soup = BeautifulSoup(r.text)
+    openIdForm = soup.find(id="openidForm")
+
+    if openIdForm == None:
+      l.error("can't find the login form, aborting")
+      quit()
+
+    payload = {}
+    for n in openIdForm.find_all("input"):
+      if "name" in n.attrs.keys():
+        l.debug("adding to payload: %s -  %s" % (n["name"], n["value"]))
+        payload[n["name"]] = n["value"]
+
+    r = self.session.post("https://steamcommunity.com/openid/login", data=payload)
+
+    if r.url.startswith(self.SCRAP_BASE_URL):
+      l.info("LOGIN SUCCESS!")
+    else:
+      l.error("LOGIN FAIL, url is not %s, but %s" % (SCRAP_BASE_URL, r.url))
+      quit()
+
+
+if __name__ == "__main__":
+  a = ScrapTfScraper()
+  a.dumpItems()
